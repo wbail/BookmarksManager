08e697f9192f53df1ac21777bcdba44db20701cc
wbail <guilhermedanbail@gmail.com>
2021-11-28 12:00:39 +0000

    feat: add context and repository
    base repositoy

---
+++ BookmarksManager.Persistence/BookmarksManagerDbContext.cs
@@ -0,0 +1,20 @@
+﻿using BookmarksManager.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookmarksManager.Persistence;
+
+public class BookmarksManagerDbContext : DbContext
+{
+    public BookmarksManagerDbContext(DbContextOptions<BookmarksManagerDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<BookmarkBar> BookmarkBar { get; set; }
+    public DbSet<Child> Child { get; set; }
+    public DbSet<MetaInfo> MetaInfo { get; set; }
+    public DbSet<Other> Other { get; set; }
+    public DbSet<Root> Root { get; set; }
+    public DbSet<Roots> Roots { get; set; }
+    public DbSet<Synced> Synced { get; set; }
+}

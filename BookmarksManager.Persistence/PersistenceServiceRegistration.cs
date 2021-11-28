e01b5e8dca75041fa990836fe7475f0f0dd4c133
wbail <guilhermedanbail@gmail.com>
2021-11-28 12:13:32 +0000

    feat: add service registration

---
+++ BookmarksManager.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using BookmarksManager.App.Contracts.Persistence;
+using BookmarksManager.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BookmarksManager.Persistence;
+
+public static class PersistenceServiceRegistration
+{
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<BookmarksManagerDbContext>(options =>
+            options.UseSqlite(configuration.GetConnectionString("BookmarksManager")));
+
+        services.AddScoped(typeof(IAsyncRepository<>), typeof(BaseRepository<>));
+
+        return services;
+    }
+}

8cae10927753daeff1ed88dfb10f9d4a1844c172
wbail <guilhermedanbail@gmail.com>
2022-02-05 10:31:52 +0000

    refact: add single responsability

--- BookmarksManager.App/Services/TestLinkService.cs
+++ BookmarksManager.App/Services/TestLinkService.cs
@@ -1,60 +1,94 @@
 ﻿using BookmarksManager.App.Contracts.Services;
 using Microsoft.Extensions.Logging;
 using System.Diagnostics;
+using System.Net;
 
 namespace BookmarksManager.App.Services;
 
 public class TestLinkService : ITestLinkService
 {
+    #region Private Fields
+
     private readonly IHttpClientService _httpClientService;
     private readonly ILogger<TestLinkService> _logger;
+    private Stopwatch _stopWatch;
+    private Dictionary<string, bool> _dictionary;
+
+    #endregion
+
+    #region Constructors
 
     public TestLinkService(IHttpClientService httpClientService, ILogger<TestLinkService> logger)
     {
         _httpClientService = httpClientService;
         _logger = logger;
+        _dictionary = new Dictionary<string, bool>();
     }
+
+    #endregion
+
+    #region Public Methods
 
     public async Task<Dictionary<string, bool>> TestLinksAsync(IEnumerable<string> urls)
     {
-        var dictionary = new Dictionary<string, bool>();
-
-        _logger.LogInformation($"Start: {DateTime.UtcNow}");
-        var stopWatch = new Stopwatch();
-        stopWatch.Start();
+        _logger.LogDebug($"Start: {DateTime.UtcNow}");
+        _stopWatch = new Stopwatch();
+        _stopWatch.Start();
 
         foreach (var url in urls)
         {
-            var result = await _httpClientService.GetAsync(url);
-
-            if ((int)result.StatusCode < 400)
-            {
-                dictionary.Add(url, true);
-            }
-            else
-            {
-                dictionary.Add(url, false);
-            }
+            await TestLinkAndAddToDictionary(url);
         }
 
-        stopWatch.Stop();
-        _logger.LogInformation($"End: {DateTime.UtcNow}");
-        _logger.LogInformation($"Time elapsed: {stopWatch.Elapsed}");
+        _stopWatch.Stop();
+        _logger.LogDebug($"End: {DateTime.UtcNow}");
+        _logger.LogInformation($"Time elapsed: {_stopWatch.Elapsed}");
+        _stopWatch.Reset();
 
-        return dictionary;
+        return _dictionary;
     }
 
     public async Task<Dictionary<string, bool>> TestOneLinkAsync(string url)
     {
+        await TestLinkAndAddToDictionary(url);
+
+        return _dictionary;
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    private async Task<HttpResponseMessage> TestLinkAsync(string url)
+    {
         var result = await _httpClientService.GetAsync(url);
 
-        var dictionary = new Dictionary<string, bool>();
-
-        if ((int)result.StatusCode < 400)
-        {
-            dictionary.Add(url, true);
-        }
-
-        return dictionary;
+        return result;
     }
+
+    private async Task TestLinkAndAddToDictionary(string url)
+    {
+        var result = await TestLinkAsync(url);
+
+        if (IsSuccessStatusCode(result.StatusCode))
+        {
+            AddValueToDictionary(url, true);
+        }
+        else
+        {
+            AddValueToDictionary(url, false);
+        }
+    }
+
+    private void AddValueToDictionary(string url, bool value)
+    {
+        _dictionary.Add(url, value);
+    }
+
+    private bool IsSuccessStatusCode(HttpStatusCode statusCode)
+    {
+        return (int)statusCode >= 200 && (int)statusCode < 400;
+    }
+
+    #endregion
 }
